925f00614767d3a398a69a202fb46227b8e86b1d
Joao Lucas <joaopedroramosdelucas10@gmail.com>
2024-01-04 18:03:18 -0300

    Update :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
+import Pesquisa from './Componentes/Pesquisa';
 import Header from './Componentes/Header';
 
 const AppContainer = styled.div`
+    overflow: auto;
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(90deg, #000000, 35%, #2e2c2c);
@@ -12,6 +14,7 @@ function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
---
+++ src/Componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import Livro from '../../Imagens/livro.png'
+import Livro1 from '../../Imagens/livro2.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 0, src: Livro},
+    {nome: 'Liderança em UX', id: 1, src: Livro},
+    {nome: 'JavaScript Assertivo', id: 2, src: Livro1},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src: Livro},
+    {nome: 'Aventureitos do JavaScript', id: 4, src: Livro1}
+]
